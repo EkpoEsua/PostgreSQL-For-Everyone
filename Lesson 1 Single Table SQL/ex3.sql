8589177818785a31801ad61d80e8ec3703dcec1e
Esua Ekpo <esuaekpo@yahoo.com>
2021-07-14 08:08:58 +0100

    implement lesson 1 exercises

---
+++ Lesson 1 Single Table SQL/ex3.sql
@@ -0,0 +1,12 @@
+CREATE TABLE ages ( 
+  name VARCHAR(128), 
+  age INTEGER
+);
+
+DELETE FROM ages;
+INSERT INTO ages (name, age) VALUES ('Aulay', 35);
+INSERT INTO ages (name, age) VALUES ('Giane', 18);
+INSERT INTO ages (name, age) VALUES ('Keane', 35);
+INSERT INTO ages (name, age) VALUES ('Nathanael', 32);
+INSERT INTO ages (name, age) VALUES ('Reng', 15);
+INSERT INTO ages (name, age) VALUES ('Zoubaeir', 14);
